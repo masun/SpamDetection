d6532d3e77d11b0cd1dca9898a034fbe2c1b0f07
Edward Fernandez <edwardfernandez26@gmail.com>
2017-03-12 11:03:48 -0400

    Ya se pueden hacer los diagramas de cajas para cada top-topic.

--- graficas.py
+++ graficas.py
@@ -64,7 +64,7 @@ for i in range(nro_atributos):
 vector_topTopics = [[0 for i in range(0, len(wordsbag)) ] for d in range(0, len(top_topicsL))]
 
 i = 0 # Identificar de la fuente a usar.
-for instancia in dict_datos_top: #Para cada instancia (fuente de datos)
+for instancia in dict_datos_top: #Para cada instancia.
 	for atributo in top_topicsL: # Para cada atributo de la instancia actual
 		vector_topTopics[i][int(instancia[atributo])] += 1
 		i += 1
@@ -75,19 +75,17 @@ colors = ["#EC644B", "#D2527F", "#663399", "#446CB3", "#2C3E50", "#36D7B7",
 		  "#E9D460", "#F2784B", "#D35400", "#6C7A89"]
 
 
-# Se dibuja el barchart para cada fuente con su top_topic.
-x = np.arange(len(wordsbag))     # Wordsbags de la fuente actual.
+# Se dibuja el barchart para cada fuente con su top topic.
+x = np.arange(len(wordsbag))     # Wordsbags de los top topics.
 
-# Se crean los labels labels:
-labelx = labelLegend = ()
-for i in range(0, len(wordsbag)):
-	labelx += (x[i],)
-	labelLegend += (wordsbag[i],)
-
-barchart = plt.bar(x, vector_topTopics[0], align='center', color=colors)
-plt.xticks(x, x)                  # Para que se pueda ver cada elemento del xaxis.
-plt.xlim(-0.5, len(wordsbag)-0.5) # Limite x de la grafica.
-plt.legend(x, vector_topTopics)
-plt.show()						  # Se plotea.
+# Hago el diagrama de cajas para cada top topic.
+for i in range(0, len(top_topicsL)):
+	barchart = plt.bar(x, vector_topTopics[i], align='center', color=colors)
+	plt.xticks(x, x)                  # Para que se pueda ver cada elemento del xaxis.
+	plt.xlim(-0.5, len(wordsbag)-0.5) # Limite x de la grafica.
+	plt.xlabel('Id del topic')
+	plt.ylabel('Frecuencias')
+	plt.title('Frecuencias de tweets asociado a un topico para el Top topic ' + str(i + 1))
+	plt.show()						  # Se plotea.
 
 #.-----------------------------------------------------------------------------.
