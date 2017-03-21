2ad9504867fca55a9ef9697fe4312c66c03b64fa
al3x4nd3r <alexanderstower@gmail.com>
2017-03-21 06:48:43 +0000

    Agregada nota al principio de la función. Hecho más general el script

--- hacerPrediccion.py
+++ hacerPrediccion.py
@@ -4,6 +4,7 @@ import traceback
 from weka.core.converters import Loader
 import weka.core.serialization as serialization
 from weka.classifiers import Classifier
+from weka.filters import Filter, MultiFilter
 
 # Fuentes:
 # https://github.com/fracpete/python-weka-wrapper-examples/blob/master/src/wekaexamples/classifiers/output_class_distribution.py
@@ -15,21 +16,33 @@ def predictWithWeka(csvFilenameWithInputToPredict,modelFilename):
     #   Nota: para usar sin conocer la clase, se puede colocar una clase dummy
     #   e ignorar los valores actual y error de @return results.
     #
+    #   Nota: es necesario que el archivo de nombre @csvFilenameWithInputToPredict
+    #   contenga instancias de ambas clases (spam y sanas)
+    #
     #   @csvFilenameWithInputToPredict : nombre del archivo csv con las instancias
     #                                   a predecir.
     #
     #   @modelFilename : nombre del archivo de modelo generado por weka y 
     #                    compatible con el archivo csv de entrada
     #
-    #   @return results : lista de diccionarios con los siguientes elementos
+    #   @return results : lista de diccionarios con los siguientes indices
     #                      index, actual, predicted, error y distribution
     """
     loader = Loader(classname="weka.core.converters.CSVLoader")
     cls = Classifier(jobject=serialization.read(modelFilename))
     print(cls)
     
-    test = loader.load_file(csvFilenameWithInputToPredict)
-    test.class_is_last()
+    data = loader.load_file(csvFilenameWithInputToPredict)
+    data.class_is_last()
+    
+    multi = MultiFilter()
+    remove = Filter(classname="weka.filters.unsupervised.attribute.Remove", options=["-R", "first"])
+    numericToNom = Filter(classname="weka.filters.unsupervised.attribute.NumericToNominal", options=["-R","8,11"])
+    normalize = Filter(classname="weka.filters.unsupervised.attribute.Normalize",options=["-S","1.0","-T","0.0"])
+    multi.filters = [remove, numericToNom, normalize]
+    multi.inputformat(data)
+    test = multi.filter(data)
+    
     
     results = []
     for index, inst in enumerate(test):
@@ -45,17 +58,25 @@ def predictWithWeka(csvFilenameWithInputToPredict,modelFilename):
         result["distribution"] = str(dist.tolist())
         
         results.append(result)
-        
+        print result
         
     return results
 
-if __name__ == '__main__':
+def main() :
     
-    jvm.start()
-    jvm.start(system_cp=True, packages=True)
-    
-    ifileName = "featureVectorsEntrenamientoWithTopicRetuitFav_preprocesado.csv"
+    #ifileName = "featureVectorsEntrenamientoWithTopicRetuitFav_preprocesado.csv"
+    ifileName = "predictMe.csv"
     modelFilename = "naivebayes.model"
     out = predictWithWeka(ifileName,modelFilename)
     print out
-    jvm.stop()
+
+
+if __name__ == "__main__":
+    try:
+        jvm.start()
+        jvm.start(system_cp=True, packages=True)
+        main()
+    except Exception, e:
+        print(traceback.format_exc())
+    finally:
+        jvm.stop()
