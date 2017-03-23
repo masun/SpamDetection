7e3bcac6f6503892da4a87e6524fa4a5258bb6f4
al3x4nd3r <alexanderstower@gmail.com>
2017-03-23 02:53:04 +0000

    cambios al shell-script para que funcione en c9

--- Interfaz/shell_script.sh
+++ Interfaz/shell_script.sh
@@ -1,15 +1,15 @@
 #!/bin/sh
 
 # Para el 'correcto' funcionamiento de la aplicación.
-fuser -k 8000/tcp; # Se mata el proceso que esté usando el puerto 8000.
+fuser -k 8080/tcp; # Se mata el proceso que esté usando el puerto 8080.
 sleep 1;           # Se espera un segundo para activar el servidor.
-python manage.py runserver; # Se inicia el servidor de Django
+python manage.py runserver 0.0.0.0:8080; # Se inicia el servidor de Django
 
 while true ; do
-    if lsof -Pi :8000 -sTCP:LISTEN -t >/dev/null ; then
+    if lsof -Pi :8080 -sTCP:LISTEN -t >/dev/null ; then
         echo "Running Django"
     else
         echo "Start Django"
-        python manage.py runserver;
+        python manage.py runserver 0.0.0.0:8080;
     fi
 done
